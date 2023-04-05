befbf0e44e892cdf0345ed12eea73221b1ccf0cc
Peter <peterhanggg@gmail.com>
2023-04-05 15:58:49 -0400

    Added ability to download canvas drawing

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -21,6 +21,31 @@ const Canvas = ({ width, height }: CanvasProps) => {
     isCanvasEmpty,
   } = useOnDraw();
 
+  const downloadHandler = (
+    chartRef: React.MutableRefObject<HTMLCanvasElement | null>
+  ) => {
+    if (!canvasRef.current) return;
+
+    const newCanvas = document.createElement("canvas");
+    newCanvas.width = canvasRef.current.width;
+    newCanvas.height = canvasRef.current.height;
+
+    const ctx = newCanvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.fillStyle = "#ffffff";
+    ctx.fillRect(0, 0, newCanvas.width, newCanvas.height);
+    // Draw the original canvas on the new canvas
+    ctx.drawImage(canvasRef.current, 0, 0);
+
+    const downloadLink = document.createElement("a");
+    downloadLink.setAttribute("download", "sketch.png");
+
+    const imageUrl = newCanvas.toDataURL("image/png");
+    downloadLink.setAttribute("href", imageUrl);
+    downloadLink.click();
+  };
+
   return (
     <div className="flex flex-col items-center gap-4">
       <canvas
@@ -32,6 +57,12 @@ const Canvas = ({ width, height }: CanvasProps) => {
       />
       <div className="flex items-center bg-slate-100 p-6">
         <ColorPicker setColor={setColor} color={color} />
+        <button
+          className="mx-2 w-[200px] rounded border border-gray-400 bg-white px-4 py-2 font-semibold text-gray-800 shadow hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50"
+          onClick={() => downloadHandler(canvasRef)}
+        >
+          download
+        </button>
         <button
           className="mx-2 w-[200px] rounded border border-gray-400 bg-white px-4 py-2 font-semibold text-gray-800 shadow hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50"
           onClick={clear}
